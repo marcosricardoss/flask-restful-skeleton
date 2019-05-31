ca0fc2d34ef6716e8ddd44e61b03fecce20643b1
Marcos Ricardo <marcosricardoss@gmail.com>
2019-05-31 00:54:44 -0300

    test: initializes the database in the 'app' fixture through the DBFactory. test: add the 'runner' fixture in the conftest module.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,33 @@ import tempfile
 
 import pytest
 from app import create_app
+from app.model.database import DBFactory
 
 
 @pytest.fixture
 def app():
+
+    database_fd, database_file_path = tempfile.mkstemp()
+
     app = create_app({
-        'TESTING': True        
+        'TESTING': True,
+        'DATABASE': database_file_path,
     })    
+    
+    with app.app_context():
+        DBFactory().create()        
+        
     yield app
+
+    os.close(database_fd)
+    os.unlink(database_file_path)
 
 
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
