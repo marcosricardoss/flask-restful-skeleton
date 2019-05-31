d36cd0c29d06b86e402fd077317006f29351b045
Marcos Ricardo <marcosricardoss@gmail.com>
2019-05-30 22:54:20 -0300

    test: add a test to the application factory.

---
+++ tests/test_factory.py
@@ -0,0 +1,11 @@
+from app import create_app
+
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+
+def test_index(client):
+    response = client.get('/')
+    assert response.data == b'The server is runing!'
