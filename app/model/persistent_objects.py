5d93690b642304bc92ba776932ccc5e9d5777c3a
Marcos Ricardo <marcosricardoss@gmail.com>
2019-05-31 12:50:31 -0300

    feat: creates the User persistent class

---
+++ app/model/persistent_objects.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from sqlalchemy import Column, Integer, String
+from app.model.database import DBFactory
+
+Base = DBFactory().get_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    email = Column(String(120), unique=True)
+
+    def __init__(self, name=None, email=None):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
