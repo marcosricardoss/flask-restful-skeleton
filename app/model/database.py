487999beeda73ec803aa4c2dd85c3d5a52ce59cf
Marcos Ricardo <marcosricardoss@gmail.com>
2019-05-30 23:18:51 -0300

    feat: add the 'init-db' command line, to create the table into the database.

--- app/model/database.py
+++ app/model/database.py
@@ -67,8 +67,17 @@ class DBFactory:
             session.remove()
 
 
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    DBFactory().create()
+    click.echo('Initialized the database.')      
+
+    
 def init_app(app):
     """Register database functions with the Flask app. This is called by
     the application factory.
     """
     app.teardown_appcontext(DBFactory.shutdown_session)
+    app.cli.add_command(init_db_command)
