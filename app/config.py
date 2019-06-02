f56334c667e1963b01290175a4d99be010b64ba6
Marcos Ricardo <marcosricardoss@gmail.com>
2019-06-02 12:29:09 -0300

    refactor: uses the dotenv library to load some settings on the development environment.

--- app/config.py
+++ app/config.py
@@ -1,18 +1,28 @@
 import os
 
+from dotenv import load_dotenv
+
 class Default(object):        
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    
 
 
 class Production(Default):
-    SECRET_KEY = b'\xacP=\x12\xa6\xa2\x19`\xbcu{\x0b\xe4&H\x8d'
+    """Class containing the settings of the production environment . 
+    
+    It load some values from the environment to be used in the internal Flask config.
+    """
+
+    SECRET_KEY = b'\xacP=\x12\xa6\xa2\x19`\xbcu{\x0b\xe4&H\x8d'    
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
 
-
-class Development(Default):    
-    SECRET_KEY = 'dev'        
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format("postgres", 
-                                                                            "123", 
-                                                                            "127.0.0.1", 
-                                                                            "15432", 
-                                                                            "olist_dev")
+class Development(Default):
+    """Class containing the settings of the development environment. 
+    
+    It uses the dotenv library to load some values from the .env file to environment. 
+    After that, theses values are load from the environment to be use in the internal Flask config.
+    """
+    
+    load_dotenv() # load .env file
+    SECRET_KEY = 'dev'            
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')    
