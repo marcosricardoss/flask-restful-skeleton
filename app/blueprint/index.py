13730489f0bf49db7fd0a9c3e80c76ed38e938f8
Marcos Ricardo <marcosricardoss@gmail.com>
2019-06-03 16:05:34 -0300

    doc: add doctring to the index blueprint.

--- app/blueprint/index.py
+++ app/blueprint/index.py
@@ -1,12 +1,23 @@
-import functools
+""" This module contains the 'index' Blueprint, which organize and
+group, views related  to the index endpoint of HTTP REST API.
+"""
+
 
 from flask import Blueprint
 
+
 bp = Blueprint('index', __name__, url_prefix='')
 
+
 @bp.route('/', methods=['GET'])
-def index():    
-    """Show a simple text indicating the server is running."""
-    
+def index() -> str:
+    """
+    This function is responsible to deal with requests
+    coming from index URL. It return a simple text indicating
+    the server is running.
+
+    Returns:
+        str: a text message
+    """
+
     return "The server is runing!"
-    
