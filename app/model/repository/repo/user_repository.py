11a0c5305c98bb9f045eb9a8d3e89083e9943634
Marcos Ricardo <marcosricardoss@gmail.com>
2019-06-05 16:42:49 -0300

    feat: add the user_repository module.
    This module contains the abstract class used to implements user repositories.

---
+++ app/model/repository/repo/user_repository.py
@@ -0,0 +1,41 @@
+"""This module contains the abstract class used to implements user repositories."""
+
+
+from __future__ import annotations
+from abc import abstractmethod
+
+from app.model.repository.repository import Repository
+from app.model.models import User
+
+
+class UserRepository(Repository):
+    """UserRepository is subclass of the Repository abstract class that
+    provides specific abstract methods related to the User model.
+    """
+
+    @abstractmethod
+    def get_by_username(self, username: str) -> User:
+        """Retrive a user by username.
+
+        Parameters:
+           username (str): The username of the user.
+
+        Returns:
+            User: User model object.
+        """
+
+        pass
+
+    @abstractmethod
+    def authenticate(self, username: str, password: str) -> bool:
+        """checks user authenticity by username and password.
+
+        Parameters:
+            username (str): The username of the user.
+            password (str): The password of the user.
+
+        Returns:
+            bool: A boolean indicating the user authenticity.
+        """
+
+        pass
