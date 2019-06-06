9b2f391d936bc190579f492ccb7e3857b0f09b9f
Marcos Ricardo <marcosricardoss@gmail.com>
2019-06-06 17:42:13 -0300

    feat: adds on the Repository class, implementations that are common to all its subclass.

--- app/model/repository/repository.py
+++ app/model/repository/repository.py
@@ -3,6 +3,11 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 
+from sqlalchemy import desc
+
+from app.model.database import db_session
+from app.model.models import Model
+
 
 class Repository(ABC):
     """The Repository abstract class declares the abstract methods to do operations in the
@@ -10,21 +15,21 @@ class Repository(ABC):
     of these methods.
     """
 
-    @abstractmethod
-    def get(self, model_id: int) -> object:
+    def __init__(self, model_class):
+        self.__model_class = model_class
+
+    def get(self, model_id: int) -> Model:
         """Retrieve a model register from database by its id.
 
         Parameters:
            model_id (int): Id of the model to be retrieved.
 
         Returns:
-           object: a model object.
+           Model: a model object.
         """
 
-        pass
+        return db_session.query(self.__model_class).filter_by(id=model_id).first()
 
-
-    @abstractmethod
     def get_all(self) -> list:
         """Retrieves a list of all elements in the database.
 
@@ -32,48 +37,67 @@ class Repository(ABC):
            list: a list of model objects.
         """
 
-        pass
+        return db_session.query(self.__model_class).order_by(desc(self.__model_class.id))
 
-    @abstractmethod
-    def save(self, model: object) -> None:
+    def save(self, model: Model) -> None:
         """Saves a model in the database.
 
         Parameters:
            model (Model): A model object.
         """
 
-        pass
+        db_session.commit()
 
-    @abstractmethod
-    def update(self, model: object) -> None:
+    def update(self, model: Model) -> None:
         """Update a existent model register in the database.
 
         Parameters:
-           model (object): A model object.
+           model (Model): A model object.
         """
 
-        pass
+        db_session.commit()
 
-    @abstractmethod
-    def delete(self, model: object) -> int:
+    def delete(self, model: Model) -> int:
         """Delete a existent model register in the database.
 
         Parameters:
-           model (object): A model object.
+           model (Model): A model object.
 
         Returns:
            int: the a model id that was deleted.
         """
 
-        pass
+        deleted = db_session.delete(model)
+        print(deleted)
+        db_session.commit()
 
+        return deleted
+
+    def delete_list(self, models):
+        """Delete a list of models registered in the database.
+
+        Parameters:
+           models (list): A model object list.
+
+        Returns:
+           int: the a model id that was deleted.
+        """
+
+        deleted_list = list()
+        for model in models:
+            deleted = db_session.delete(model)
+            deleted_list.append(deleted)
+
+        db_session.commit()
+
+        return deleted_list
 
     @abstractmethod
-    def is_invalid(self, model: object, editing: bool = False) -> list:
+    def is_invalid(self, model: Model, editing: bool = False) -> list:
         """Checks if a given model object is valid.
 
         Parameters:
-            user (User): The User model object.
+            model (Model): The model object.
             editing (bool): Indicates whether the validation is for an editing.
 
         Returns:
