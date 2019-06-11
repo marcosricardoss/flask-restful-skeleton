c39d4be13f6d71e38f368dc66a0b6a3440ac66c5
Marcos Ricardo <marcosricardoss@gmail.com>
2019-06-11 10:42:06 -0300

    test: adds new test on the test_user_delete.py module.

--- tests/users_blueprint/test_user_delete.py
+++ tests/users_blueprint/test_user_delete.py
@@ -3,13 +3,21 @@ import base64
 from datetime import datetime
 
 from flask import json
-from util import create_user
+from util import create_user, get_unique_id
 
 
 def test_user_delete_with_all_data_passed_returning_200_status_code(client, session, auth):
     user = create_user(session)
     endpoint = '/users/{}'.format(user.id)
+    # assert user was deleted
     response = client.delete(endpoint, headers=auth)
     assert response.status_code == 201
+    # assert the user no longer exist
     response = client.get(endpoint, headers=auth)
+    assert response.status_code == 404
+
+
+def test_user_delete_with_inexistent_user_id_returning_404_status_code(client, auth):
+    endpoint = '/users/{}'.format(get_unique_id())
+    response = client.delete(endpoint, headers=auth)
     assert response.status_code == 404
