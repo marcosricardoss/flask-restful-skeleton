0248893a6a38da9a48817719d1eadae74a4afc6f
Marcos Ricardo <marcosricardoss@gmail.com>
2019-06-11 11:38:35 -0300

    test: refactors the test_factory.py module.
    Rename the test function names.

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -6,13 +6,13 @@ from dotenv import load_dotenv
 from app import create_app
 
 
-def test_config_not_testing():
+def test_with_config_not_testing():
     load_dotenv()
     config = {'SQLALCHEMY_DATABASE_URI': os.environ.get('DATABASE_URL')}
     assert not create_app(config).testing
 
 
-def test_config_testing():
+def test_with_test_config_testing():
     load_dotenv()
     config = {
         'TESTING': True,
