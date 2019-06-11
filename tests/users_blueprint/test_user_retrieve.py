fb9c52d98a1002e3df0809f3d9abf8d266d2a68a
Marcos Ricardo <marcosricardoss@gmail.com>
2019-06-11 10:43:34 -0300

    test: add a assert in the test_user_retrieve_user_list_200_status_code test.

--- tests/users_blueprint/test_user_retrieve.py
+++ tests/users_blueprint/test_user_retrieve.py
@@ -20,19 +20,21 @@ def test_user_retrieve_with_existent_username_returning_200_status_code(client, 
     assert response.status_code == 200
 
 
-def test_user_retrieve_user_list_200_status_code(client, session, auth):    
+def test_user_retrieve_user_list_200_status_code(client, session, auth):
     user1 = create_user(session)
     user2 = create_user(session)
-    endpoint = '/users'
-    response = client.get(endpoint, headers=auth)        
-    
-    assert response.json['status'] == 'success'    
-    assert response.json['data'][0]['username'] == user2.username    
-    assert response.json['data'][1]['username'] == user1.username    
     users_count = get_users_count(session)
+
+    endpoint = '/users'
+    response = client.get(endpoint, headers=auth)
+
+    assert response.status_code == 200
+    assert response.json['status'] == 'success'
+    assert response.json['data'][0]['username'] == user2.username
+    assert response.json['data'][1]['username'] == user1.username
     assert len(response.json['data']) == users_count
     with pytest.raises(IndexError):
-        assert response.json['data'][users_count]   
+        assert response.json['data'][users_count]
 
 
 def test_user_retrieve_with_inexistent_user_id_returning_404_status_code(client, auth):
