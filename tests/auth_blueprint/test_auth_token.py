d042f1a88ae475ed3ad1abb8baf32d852286e5e2
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-12 01:15:37 -0300

    test: add helper functions to the test_auth_token.py module."

--- tests/auth_blueprint/test_auth_token.py
+++ tests/auth_blueprint/test_auth_token.py
@@ -7,18 +7,37 @@ from flask_jwt_extended import decode_token
 from ..util import create_user
 
 
-def test_auth_list_tokens_of_logged_user_returning_200_status_code(client, session):
-    user = create_user(session)
-    data = {'username': user.username, 'password': "123"}
-    
-    # login request
-    response1 = client.post('/auth/login',
+def login_request(client, data):
+    response = client.post('/auth/login',
                             data=json.dumps(data),
                             content_type='application/json')
 
-    access_token = response1.json['data']['access_token']
-    access_token_decoded = decode_token(response1.json['data']['access_token'])
-    refresh_token_decoded = decode_token(response1.json['data']['refresh_token'])
+    access_token = response.json['data']['access_token']
+    refresh_token = response.json['data']['refresh_token']
+
+    return access_token, refresh_token
+
+
+def get_tokens(username):
+    from app.model import TokenRepository
+    tokens = TokenRepository().get_user_tokens(username)
+    result = dict()
+    for token in tokens:
+        result[token.token_type] = token
+    return result
+
+
+def revoke_token(token, username):
+    from app.model import TokenRepository
+    TokenRepository().change_token_revoking(token.id, username, True)
+
+
+def test_auth_list_tokens_of_logged_user_returning_200_status_code(client, session):
+    user = create_user(session)
+    data = {'username': user.username, 'password': "123"}
+    access_token, refresh_token = login_request(client, data)
+    access_token_decoded = decode_token(access_token)
+    refresh_token_decoded = decode_token(refresh_token)
     
     # request to get token list
     response2 = client.get('/auth/token', 
@@ -35,21 +54,13 @@ def test_auth_list_tokens_of_logged_user_returning_200_status_code(client, sessi
 
 def test_auth_revoke_an_existent_token_returning_200_status_code(client, session):
     user = create_user(session)
-    data = {'username': user.username, 'password': "123"}
-    
-    # login request
-    response1 = client.post('/auth/login',
-                           data=json.dumps(data),
-                           content_type='application/json')
-    access_token = response1.json['data']['access_token']
-   
-    # getting the user tokens
-    from app.model import TokenRepository
-    tokens = TokenRepository().get_user_tokens(user.username)
+    data = {'username': user.username, 'password': "123"}       
+    access_token, __ = login_request(client, data) 
+    tokens_models = get_tokens(user.username)
 
     # revoking the refresh token
     data = {'revoke': True}
-    response3 = client.put('/auth/token/'+str(tokens[1].id),
+    response3 = client.put('/auth/token/'+str(tokens_models['refresh'].id),
                            content_type='application/json',
                            data=json.dumps(data),
                            headers={'Authorization': 'Bearer ' + access_token})
@@ -59,31 +70,22 @@ def test_auth_revoke_an_existent_token_returning_200_status_code(client, session
 
     # checking if the refresh token is revoked
     from app.model import TokenRepository
-    tokens = TokenRepository().get_user_tokens(user.username)
-    assert tokens[1].revoked
+    tokens_models = get_tokens(user.username)
+    assert tokens_models['refresh'].revoked
 
 
 def test_auth_unrevoke_an_existent_token_returning_200_status_code(client, session):
     user = create_user(session)
-    data = {'username': user.username, 'password': "123"}
-    
-    # login request
-    response1 = client.post('/auth/login',
-                            data=json.dumps(data),
-                            content_type='application/json')
-    access_token = response1.json['data']['access_token']
-
-    # getting the user tokens
-    from app.model import TokenRepository
-    token_repository = TokenRepository()
-    tokens = token_repository.get_user_tokens(user.username)
+    data = {'username': user.username, 'password': "123"}       
+    access_token, __ = login_request(client, data) 
+    tokens_models = get_tokens(user.username)
     
     # revoking the refresh token
-    token_repository.change_token_revoking(tokens[1].id, user.username, True)
+    revoke_token(tokens_models['refresh'], user.username)
 
     # unrevoking the refresh token
     data = {'revoke': False}
-    response3 = client.put('/auth/token/'+str(tokens[1].id),
+    response3 = client.put('/auth/token/'+str(tokens_models['refresh'].id),
                            content_type='application/json',
                            data=json.dumps(data),
                            headers={'Authorization': 'Bearer ' + access_token})
@@ -93,9 +95,25 @@ def test_auth_unrevoke_an_existent_token_returning_200_status_code(client, sessi
 
     # checking if the refresh token is unrevoked
     from app.model import TokenRepository
-    tokens = TokenRepository().get_user_tokens(user.username)
-    assert not tokens[1].revoked
+    tokens_models = get_tokens(user.username)
+    assert not tokens_models['refresh'].revoked
+
+
+def test_auth_revoke_and_try_access_a_protected_url_returning_200_status_code(client, session):    
+    user = create_user(session)
+    data = {'username': user.username, 'password': "123"}       
+    access_token, __ = login_request(client, data) 
+    tokens_models = get_tokens(user.username)
     
+    # revoking the refresh token
+    revoke_token(tokens_models['access'], user.username)
+
+    # try to access a protected url
+    endpoint = '/auth/token'
+    response = client.get(endpoint, headers={'Authorization': 'Bearer '+access_token})
+    assert response.status_code == 401
+    assert response.json['msg'] == 'Token has been revoked'
+
 
 def test_auth_revoke_with_an_inexistent_token_id_returning_400_status_code(client, auth):
     response = client.put('/auth/token/9999999',
@@ -107,20 +125,12 @@ def test_auth_revoke_with_an_inexistent_token_id_returning_400_status_code(clien
 
 def test_auth_revoke_without_data_returning_400_status_code(client, session):
     user = create_user(session)
-    data = {'username': user.username, 'password': "123"}
-    
-    # login request
-    response1 = client.post('/auth/login',
-                            data=json.dumps(data),
-                            content_type='application/json')
-    access_token = response1.json['data']['access_token']
-    
-    # getting the user tokens
-    from app.model import TokenRepository
-    tokens = TokenRepository().get_user_tokens(user.username)
+    data = {'username': user.username, 'password': "123"}       
+    access_token, __ = login_request(client, data) 
+    tokens_models = get_tokens(user.username)
     
     # revoking the refresh token
-    response = client.put('/auth/token/'+str(tokens[1].id),
+    response = client.put('/auth/token/'+str(tokens_models['refresh'].id),
                           content_type='application/json',
                           headers={'Authorization': 'Bearer ' + access_token})
     assert response.status_code == 400
@@ -129,21 +139,13 @@ def test_auth_revoke_without_data_returning_400_status_code(client, session):
 
 def test_auth_revoke_with_invalid_revoke_vaue_returning_400_status_code(client, session):
     user = create_user(session)
-    data = {'username': user.username, 'password': "123"}
-    
-    # login request
-    response1 = client.post('/auth/login',
-                            data=json.dumps(data),
-                            content_type='application/json')
-    access_token = response1.json['data']['access_token']
-    
-    # getting the user tokens
-    from app.model import TokenRepository
-    tokens = TokenRepository().get_user_tokens(user.username)
-    
+    data = {'username': user.username, 'password': "123"}       
+    access_token, __ = login_request(client, data) 
+    tokens_models = get_tokens(user.username)
+
     # revoking the refresh token
     data = {'revoke': "xxxxx"}
-    response = client.put('/auth/token/'+str(tokens[1].id),
+    response = client.put('/auth/token/'+str(tokens_models['refresh'].id),
                           content_type='application/json',
                           data=json.dumps(data),
                           headers={'Authorization': 'Bearer ' + access_token})
