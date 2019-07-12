3f964b54a74f5fc71f0acd2608e9f442a32046ca
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-12 00:13:33 -0300

    feat: update the check_if_token_in_blacklist() callback method.
    Uses the TokenRepository to check if a token is revoke or not.

--- app/authentication/__init__.py
+++ app/authentication/__init__.py
@@ -1,6 +1,5 @@
 """Initialize the JWTManager by setting functionality to manage the token."""
 
-blacklist = set() # TODO: saves in the database
 
 def init(jwt):
     """Initialize the JWTManager.
@@ -10,12 +9,11 @@ def init(jwt):
     """
 
     @jwt.token_in_blacklist_loader
-    def check_if_token_in_blacklist(decrypted_token):
+    def check_if_token_in_blacklist(decoded_token):
         """Callback to check if a token is in the blacklist.
         
         Parameters:    
             decrypted_token (dict): a jwt token decrypted into a dictionary.
         """
-
-        jti = decrypted_token['jti']
-        return jti in blacklist
+        from app.model import TokenRepository
+        return TokenRepository().is_token_revoked(decoded_token)
