1985bfbe69e041bc993943809e8e5cf71a623a1f
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-14 00:25:26 -0300

    refactor: rename the app/commands/__init__.py to app/commands.py

--- app/commands.py
+++ app/commands.py
@@ -4,7 +4,7 @@ from flask.cli import with_appcontext
 
 def register_commands(app):
     """Add commands to the line command input.
-    
+
     Parameters:    
         app (flask.app.Flask): The application instance.
     """
@@ -19,19 +19,18 @@ def register_commands(app):
 def add_user_command(username: str, password: str) -> None:
     """This function is executed through the 'user' line command.
     e.g.: flask user demo demo
-    
+
     Parameters:
         username (str): The username of the user.
         password (str): The password of the user.
     """
 
     add_user(username, password)
-    
-    
+
 
 def add_user(username: str, password: str) -> None:
     """Create a new user.
-    
+
     Parameters:
         username (str): The username of the user.
         password (str): The password of the user.
@@ -39,18 +38,16 @@ def add_user(username: str, password: str) -> None:
 
     from app.model import User
     from app.model import UserRepository
-    
+
     user_repository = UserRepository()
     user = User(username=username, password=password)
 
     is_invalid = user_repository.is_invalid(user)
     if not is_invalid:
         user_repository.save(user)
-        click.echo('User created.')    
+        click.echo('User created.')
     else:
         click.echo('Could not validate the user:')
         for i in is_invalid:
             key = list(i.keys())[0]
-            click.echo("{}: {}".format(key,i[key]))
-            
-            
+            click.echo("{}: {}".format(key, i[key]))
